eb8e1598b05449e8cb9a0856ddf33ed2783f230f
Alex Subota <as.subota@gmail.com>
2015-02-05 23:23:34 +0200

    - Service injections added - Gitignore added

--- js/crop.js
+++ js/crop.js
@@ -1,26 +1,42 @@
 (function (win, doc) {
-	var app = angular.module('ImgCropApp', []);
-	var mainCtrl = app.controller('AppCtrl', function ($scope) {
-		$scope.imgWidth = 640;
-		$scope.imgHeight = 480;
-		$scope.imgSrc = "http://lorempixel.com";
-		$scope.imgTheme = "nature";
+	var app = angular.module('imgCropApp', []);
 
-		$scope.coords = {
+	app.factory('ImgData', function () {
+		return {
+			width: 640,
+			height: 480,
+			src: "http://lorempixel.com",
+			theme: "nature"
+		};
+	});
+
+	app.factory('BoundBoxData', function () {
+		return {
 			x0: 0, y0: 0,
 			x1: 0, y1: 0
 		};
+	});
+
+	var imgEditor = app.controller('ImgEditorCtrl', function ($scope, ImgData, BoundBoxData) {
+		$scope.imgData = ImgData;
+		$scope.bbox = BoundBoxData;
 
 		$scope.getImgSrc = function () {
 			var parts = [
-				this.imgSrc,
-				this.imgWidth,
-				this.imgHeight,
-				this.imgTheme
+				this.imgData.src,
+				this.imgData.width,
+				this.imgData.height,
+				this.imgData.theme
 			];
 
 			return parts.join('/')
 		}
+	});
+	var imgLoader = app.controller('ImgLoaderCtrl', function ($scope, ImgData) {
+		$scope.model = ImgData;
+	});
+	var boundBoxCoords = app.controller('BoundBoxCtrl', function ($scope, BoundBoxData) {
+		$scope.bbox = BoundBoxData;
 	});
 })(window, document);
 
