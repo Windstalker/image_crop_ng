ef32fcd646bb2e853081d5fdcd92414a57ad74e3
Alex Subota <as.subota@gmail.com>
2015-02-08 02:16:16 +0200

    drag logic implementation start for bb-points

--- js/crop.js
+++ js/crop.js
@@ -1,4 +1,11 @@
 (function (win, doc) {
+	doc.onselectstart = function () {
+		return false;
+	};
+	doc.ondragstart = function () {
+		return false;
+	};
+
 	var app = angular.module('imgCropApp', []);
 
 	app.factory('ImgData', function () {
@@ -12,8 +19,8 @@
 
 	app.factory('BoundBoxData', function () {
 		return {
-			x0: 0, y0: 0,
-			x1: 0, y1: 0
+			x: 0, y: 0,
+			width: 0, height: 0
 		};
 	});
 
@@ -23,24 +30,69 @@
 			replace: true,
 			scope: true,
 			templateUrl: 'templates/autocrop.html',
-			controller: function ($scope) {
-				$scope.prop = 'property';
-				$scope.meth = function () {
-					console.log(this.prop);
+			controller: function ($scope, $document, BoundBoxData) {
+				console.log($document);
+				$scope.activePoint = null;
+				$scope.activePointOffset = null;
+				$scope.startCoord = {x: 0, y: 0};
+				$scope.lastCoord = {x: 0, y: 0};
+				$scope.bbox = BoundBoxData;
+
+				$scope.onBBPointDragStart = function (el, event) {
+					console.log('drag start');
+					var x = event.screenX,
+						y = event.screenY;
+					$scope.activePoint = el;
+					$scope.activePointOffset = el
+						.getAttribute('data-offset')
+						.split(',')
+						.map(function (item) {
+							return parseInt(item);
+						});
+					$scope.startCoord.x = x;
+					$scope.startCoord.y = y;
+
+					$scope.lastCoord.x = x;
+					$scope.lastCoord.y = y;
+
+					$document.bind('mousemove.bbpoint', function (e) {
+						$scope.onBBPointMove(e);
+					});
+					$document.bind('mouseup.bbpoint', function (e) {
+						$scope.onBBPointDragEnd(e);
+					});
+				};
+				$scope.onBBPointMove = function (e) {
+					if (!!$scope.activePoint) {
+						console.log('drag move');
+						$scope.bbox.x += e.screenX - $scope.lastCoord.x;
+						$scope.bbox.y += e.screenY - $scope.lastCoord.y;
+						$scope.lastCoord.x = e.screenX;
+						$scope.lastCoord.y = e.screenY;
+						console.log($scope.bbox);
+					}
+				};
+				$scope.onBBPointDragEnd = function (e) {
+					if (!!$scope.activePoint) {
+						$scope.activePoint = null;
+						$scope.activePointOffset = null;
+						console.log('drag end');
+					}
+					$document.unbind('mousemove.bbpoint');
+					$document.unbind('mouseup.bbpoint');
 				}
 			}
 		};
 	});
 
-	app.directive('bbPoint', function () {
+	app.directive('bbPoint', function ($document) {
 		return {
 			restrict: 'A',
 			require: '^autocrop',
 			scope: true,
-			link: function (scope, element, attrs, autocropCtrl) {
-				element.bind('click', function () {
-					console.log('onclick');
-					scope.meth();
+			link: function (scope, element) {
+				element.bind('mousedown', function (e) {
+					scope.onBBPointDragStart(this, e);
 				});
 			}
 		}
